255dc55b848f30f2de33e5f7c9f24f805f350717
antoineafonin <antoineafonin@gmail.com>
2024-12-04 13:29:47 -0500

    function getAvailableLetters and getRandomWord is done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        String secret = "hello";
+        String secret = getRandomWord();
+        System.out.printf("The random word is %s\n", secret);
         String letterGuessed = "lpapahepo";
         StringBuilder guessedLetters = new StringBuilder(letterGuessed);
 
@@ -36,27 +43,55 @@ public class Main {
         }
     }
 
-    public static void getAvailableLetters(String secret, String availableLetters) {
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    public static String getAvailableLetters(String secret) {
+        StringBuilder alphabet = new StringBuilder("abcdefghijklmnopqrstuvwxyz");
 
         for (int i = 0; i < secret.length(); i++) {
             boolean letterFound = false;
-            for (int j = 0; j < availableLetters.length(); j++) {
-                if (secret.charAt(i) == availableLetters.charAt(j)) {
+            for (int j = 0; j < alphabet.length(); j++) {
+                if (secret.charAt(i) == alphabet.charAt(j)) {
                     letterFound = true;
                     break;
                 }
             }
 
             if (letterFound) {
-                alphabet.replace(secret.charAt(i), "");
-                availableLetters = alphabet;
+                char letterToRemove = secret.charAt(i);
+                int indexToRemove = alphabet.indexOf(String.valueOf(letterToRemove));
+
+                if (indexToRemove != -1) {
+                    alphabet.deleteCharAt(indexToRemove);
+                }
             }
         }
+
+        return alphabet.toString();
     }
 
     public static String getRandomWord() {
-        return " ";
+        Random rand = new Random();
+        ArrayList<String> wordsList = new ArrayList<>();
+        try {
+            File words = new File("../hangman/src/words.txt");
+            Scanner reader = new Scanner(words);
+            while (reader.hasNextLine()) {
+                wordsList.add(reader.nextLine().trim());
+            }
+
+            reader.close();
+
+            if (!wordsList.isEmpty()) {
+                int randomIndex = rand.nextInt(100);
+                return wordsList.get(randomIndex);
+            } else {
+                System.out.println("The file is empty");
+                return "";
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+            return "";
+        }
     }
 
     public static void hangman(String secret) {
