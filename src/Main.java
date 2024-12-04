cb1224b6dde98437fb47291f63efe15d49574a5b
antoineafonin <antoineafonin@gmail.com>
2024-12-04 09:34:06 -0500

    function getGuessedWord and isLetterGuessed is done

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,62 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String secret = "hello";
+        String letterGuessed = "lpapahepo";
+        StringBuilder guessedLetters = new StringBuilder(letterGuessed);
+
+        System.out.println(isLetterGuessed(secret, letterGuessed));
+        getGuessedWord(secret, letterGuessed, guessedLetters);
+
+        System.out.println(guessedLetters);
     }
 
-    public static void isWordGuessed(String secret, String letters_guessed) {
+    public static boolean isLetterGuessed(String secret, String lettersGuessed) {
+        for (int i = 0; i < secret.length(); i++) {
+            for (int j = 0; j < lettersGuessed.length(); j++) {
+                if (secret.charAt(i) == lettersGuessed.charAt(j)) {
+                    return true;
+                }
+            }
+        }
 
+        return false;
     }
 
-    public static void getGuessedWord(String secret, String letters_guessed) {
+    public static void getGuessedWord(String secret, String lettersGuessed, StringBuilder guessedLetters) {
+        guessedLetters.setLength(0);
+
+        guessedLetters.append("_".repeat(secret.length()));
+
+        for (int i = 0; i < secret.length(); i++) {
+            char currentChar = secret.charAt(i);
+
+            if (lettersGuessed.indexOf(currentChar) != -1) {
+                guessedLetters.setCharAt(i, currentChar);
+            }
+        }
     }
 
-    public static void getAvailableLetters(String secret, String available_letters) {
+    public static void getAvailableLetters(String secret, String availableLetters) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+        for (int i = 0; i < secret.length(); i++) {
+            boolean letterFound = false;
+            for (int j = 0; j < availableLetters.length(); j++) {
+                if (secret.charAt(i) == availableLetters.charAt(j)) {
+                    letterFound = true;
+                    break;
+                }
+            }
+
+            if (letterFound) {
+                alphabet.replace(secret.charAt(i), "");
+                availableLetters = alphabet;
+            }
+        }
     }
 
-    public static String getWord() {
-
+    public static String getRandomWord() {
+        return " ";
     }
 
     public static void hangman(String secret) {
