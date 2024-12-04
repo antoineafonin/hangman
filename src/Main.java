85f8c4bf43635535971107162b99845407400215
antoineafonin <antoineafonin@gmail.com>
2024-12-03 21:55:12 -0500

    declared functions

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,23 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static void isWordGuessed(String secret, String letters_guessed) {
+
+    }
+
+    public static void getGuessedWord(String secret, String letters_guessed) {
+    }
+
+    public static void getAvailableLetters(String secret, String available_letters) {
+
+    }
+
+    public static String getWord() {
+
+    }
+
+    public static void hangman(String secret) {
+
+    }
 }
