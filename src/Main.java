64ffd145f0a0343dc6970675bce082bbfbb0af64
antoineafonin <antoineafonin@gmail.com>
2025-06-19 16:14:44 +0100

    words.txt

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,27 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public static void hangman() {}
+
+    public static String getRandomWord() {
+
+    }
+
+    public static boolean isWordGuessed(String secret, String lettersGuessed) {
+        int found = 0;
+
+        for (int i = 0; i < secret.length(); i++) {
+            for (int j = 0 ; j < lettersGuessed.length(); i++) {
+                if (secret.charAt(i) == lettersGuessed.charAt(j)) {
+                    found++;
+                    break;
+                }
+            }
+        }
+
+        return found >= secret.length()
+    }
+
+    public static void getAvailableLetters(String lettersGuessed, String availableLetters) {}
 }
