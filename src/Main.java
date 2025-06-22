814fa8189efdbfe6fd370b6bbe7789ff4db1f110
antoineafonin <antoineafonin@gmail.com>
2025-06-22 16:31:32 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        String secret = WordChoice.getRandomWord();
 
-        if (secret == null) {
-            System.out.println("The program is forced to suspend its work. Bye, Pinocchios.");
-            return;
-        }
-        HangmanGame.start(secret);
     }
 }
