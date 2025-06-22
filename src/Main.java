602c1128f168007237daee990e479151173d8e66
antoineafonin <antonafonin567@gmail.com>
2025-06-22 16:42:21 +0100

    test for email

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
+        String secret = WordChoice.getRandomWord();
 
+        if (secret == null) {
+            System.out.println("The program is forced to suspend its work. Bye, Pinocchios.");
+            return;
+        }
+        HangmanGame.start(secret);
     }
 }
